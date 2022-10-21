044a376998fc4a3669962f78c8cb31ca6254d4ed
Roberto Alaniz <roberto.alaniz3002@gmail.com>
2022-10-21 18:57:55 -0500

    emotionCache added in the app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from "@mui/material";
+import { lightTheme } from '../theme/lightTheme';
+import createEmotionCache from "../utils/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+const clientSideEmotionCache = createEmotionCache();
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component,
+  emotionCache = clientSideEmotionCache, pageProps }: any) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp
