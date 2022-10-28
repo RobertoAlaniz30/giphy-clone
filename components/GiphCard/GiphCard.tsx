54a2281381dd6fa9059af484d915fdca95cb4216
Roberto Alaniz <roberto.alaniz3002@gmail.com>
2022-10-28 10:05:17 -0500

    img changed for Image next component

--- components/GiphCard/GiphCard.tsx
+++ components/GiphCard/GiphCard.tsx
@@ -4,6 +4,8 @@ import {
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import styles from "./GiphCard.module.scss"
 import { parsedGiphObject } from "../../interfaces/interfaces"
+import Image from 'next/image'
+
 interface GiphCardProps {
     src: string,
     onClick?: () => void,
@@ -13,7 +15,7 @@ const GiphCard: React.FC<GiphCardProps> = ({ src, onClick, item }): JSX.Element 
 
     return (
         <div className={styles.giphCard_container} >
-            <img src={src} />
+            <Image src={src} />
             <Button variant="contained"
                 onClick={onClick}
                 color={`${item?.selected ? "error" : "inherit"}`}
